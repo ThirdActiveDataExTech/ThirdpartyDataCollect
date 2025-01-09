410863cec5c0a3ae34e404816edd6cc64cc1bc75
eunmiGo <148742489+eunmiGo@users.noreply.github.com>
2025-01-09 16:43:51 +0900

    [동영상 댓글 수집]
    - 동영상 댓글 수집하는 함수 작성
    - 클래스 생성

--- src/youtubeCollector/youtube_api.py
+++ src/youtubeCollector/youtube_api.py
@@ -12,60 +12,73 @@ import mkFilePath
 scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
 
-def get_search_res(search_term):
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+class YouTubeApi:
+    def __init__(self):
+        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-    api_service_name = "youtube"
-    api_version = "v3"
+        api_service_name = "youtube"
+        api_version = "v3"
+        self.youtube = googleapiclient.discovery.build(
+            api_service_name, api_version, developerKey=config.UserIdentifyCode.api_key)
 
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey=config.UserIdentifyCode.api_key)
+    def get_search_res(self, search_term):
+        request = self.youtube.search().list(
+            part="snippet",
+            maxResults=1,
+            order="viewCount",
+            q=search_term,
+            regionCode="KR",
+            relevanceLanguage="ko",
+            type="video"
+        )
+        response = request.execute()
 
-    request = youtube.search().list(
-        part="snippet",
-        maxResults=1,
-        order="viewCount",
-        q=search_term,
-        regionCode="KR",
-        relevanceLanguage="ko",
-        type="video"
-    )
-    response = request.execute()
+        return response
 
-    return response
+    def get_video_ids(self, search_term):
+        video_list = self.get_search_res(search_term)
+        video_ids = [item["id"]["videoId"] for item in video_list["items"]]
+        return video_ids
 
+    def get_video_script(self):
+        video_ids = self.get_video_ids("sbs 드라마")
 
-def get_video_ids(search_term):
-    video_ids = [item["id"]["videoId"] for item in get_search_res(search_term)["items"]]
-    return video_ids
+        for video_id in video_ids:
+            # fh = io.FileIO("YOUR_FILE", "wb")
 
+            transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+            transcript = transcript_list.find_transcript(['ko'])
 
-def get_video_script():
-    video_ids = get_video_ids("sbs 드라마")
+            # 번역 기능
+            # translated_transcript = transcript.translate('ko')
+            script = transcript.fetch()
+            print(script)
 
-    for video_id in video_ids:
-        fh = io.FileIO("YOUR_FILE", "wb")
+            sentences = []
+            for sentence in script:
+                text = sentence['text'].replace('\n', ' ')
+                sentences.append(text)
 
-        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
-        transcript = transcript_list.find_transcript(['ko'])
+            # fh.write(transcript)
+            # download = MediaIoBaseDownload(fh, request)
+            # complete = False
+            # while not complete:
+            #     status, complete = download.next_chunk()
 
-        # 번역 기능
-        # translated_transcript = transcript.translate('ko')
-        script = transcript.fetch()
-        print(script)
+    def get_reply(self, search_term):
+        for video_id in self.get_video_ids(search_term):
+            request = self.youtube.commentThreads().list(
+                part="snippet,replies",
+                maxResults=5,
+                videoId=video_id
+            )
+            response = request.execute()
 
-        sentences = []
-        for sentence in script:
-            text = sentence['text'].replace('\n', ' ')
-            sentences.append(text)
-
-        # fh.write(transcript)
-        # download = MediaIoBaseDownload(fh, request)
-        # complete = False
-        # while not complete:
-        #     status, complete = download.next_chunk()
+            print(response)
 
 
 if __name__ == "__main__":
-    get_video_ids("sbs 드라마")
-    # get_video_script()
+    youtube_api = YouTubeApi()
+    print(youtube_api.get_video_ids("sbs 드라마"))
+    # youtube_api.get_video_script()
+    # youtube_api.get_reply()
