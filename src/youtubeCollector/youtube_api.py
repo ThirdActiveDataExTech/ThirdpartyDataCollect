ad1bec1a9ca2492ca7265d5d3543034e87079fb0
eunmiGo <148742489+eunmiGo@users.noreply.github.com>
2025-01-10 16:24:15 +0900

    [수집 항목 추가]
    - 조회수 수집 기능 추가(get_video_viewcount)
    - 좋아요 수 수집 기능 추가(get_video_like_count)
    - 영상 카테고리 태그 기능 추가(get_video_tag)

--- src/youtubeCollector/youtube_api.py
+++ src/youtubeCollector/youtube_api.py
@@ -1,27 +1,41 @@
-import os
-
 import googleapiclient.discovery
 import googleapiclient.errors
 import config.conf as config
-import google_auth_oauthlib.flow
 
 from youtube_transcript_api import YouTubeTranscriptApi
 
-import mkFilePath
 
-scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+def get_video_script(video_id):
+    # fh = io.FileIO("YOUR_FILE", "wb")
+
+    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+    transcript = transcript_list.find_transcript(['ko'])
+
+    # 번역 기능
+    # translated_transcript = transcript.translate('ko')
+    script = transcript.fetch()
+    print(script)
+
+    sentences = []
+    for sentence in script:
+        text = sentence['text'].replace('\n', ' ')
+        sentences.append(text)
+
+    # fh.write(transcript)
+    # download = MediaIoBaseDownload(fh, request)
+    # complete = False
+    # while not complete:
+    #     status, complete = download.next_chunk()
+    return sentences
 
 
 class YouTubeApi:
     def __init__(self, search_term):
         self.search_term = search_term
-        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
         self.api_service_name = "youtube"
         self.api_version = "v3"
         self.youtube = googleapiclient.discovery.build(
             self.api_service_name, self.api_version, developerKey=config.UserIdentifyCode.api_key)
-        self.client_secrets_file = config.UserIdentifyCode.secrets_user_identify
 
     def get_video_list(self):
         request = self.youtube.search().list(
@@ -42,71 +56,57 @@ class YouTubeApi:
         video_ids = [item["id"]["videoId"] for item in video_list["items"]]
         return video_ids
 
-    def get_video_script(self):
-        video_ids = self.get_video_ids()
+    def get_reply(self, video_id):
+        request = self.youtube.commentThreads().list(
+            part="snippet,replies",
+            maxResults=5,
+            videoId='QbLhA5v4GfU'
+        )
+        response = request.execute()
+        return response
 
-        for video_id in video_ids:
-            # fh = io.FileIO("YOUR_FILE", "wb")
+    def get_video_inspect(self, video_id):
+        request = self.youtube.videos().list(
+            part="snippet,contentDetails,statistics",
+            id=video_id
+        )
+        response = request.execute()
+        return response
 
-            transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
-            transcript = transcript_list.find_transcript(['ko'])
+    def get_video_thumbnail(self, video_id):
+        video_inspect = self.get_video_inspect(video_id)
+        thumbnail = [item["snippet"]["thumbnails"] for item in video_inspect["items"]]
+        return thumbnail
 
-            # 번역 기능
-            # translated_transcript = transcript.translate('ko')
-            script = transcript.fetch()
-            print(script)
+    def get_video_viewcount(self, video_id):
+        video_inspect = self.get_video_inspect(video_id)
+        viewcount = [item["statistics"]["viewCount"] for item in video_inspect["items"]]
+        return viewcount
 
-            sentences = []
-            for sentence in script:
-                text = sentence['text'].replace('\n', ' ')
-                sentences.append(text)
+    def get_video_like_count(self, video_id):
+        video_inspect = self.get_video_inspect(video_id)
+        like_count = [item["statistics"]["likeCount"] for item in video_inspect["items"]]
+        return like_count
 
-            # fh.write(transcript)
-            # download = MediaIoBaseDownload(fh, request)
-            # complete = False
-            # while not complete:
-            #     status, complete = download.next_chunk()
-
-    def get_reply(self):
-        for video_id in self.get_video_ids():
-            print(video_id)
-            request = self.youtube.commentThreads().list(
-                part="snippet,replies",
-                maxResults=5,
-                videoId='QbLhA5v4GfU'
-            )
-            response = request.execute()
-
-            print(response)
-
-    def get_video_inspect(self):
-        for video_id in self.get_video_ids():
-            flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-                self.client_secrets_file, scopes)
-            credentials = flow.run_local_server()
-            youtube = googleapiclient.discovery.build(
-                self.api_service_name, self.api_version, credentials=credentials)
-
-            request = youtube.videos().list(
-                part="snippet,contentDetails,statistics",
-                id=video_id
-            )
-            response = request.execute()
-
-            print(response)
-
-    def get_video_thumbnail(self):
-        video_list = self.get_video_list()
-        thumbnail = [item["snippet"]["thumbnails"] for item in video_list["items"]]
-        print(thumbnail)
+    def get_video_tag(self, video_id):
+        video_inspect = self.get_video_inspect(video_id)
+        tag = [item["snippet"]["tags"] for item in video_inspect["items"]]
+        return tag
 
 
 if __name__ == "__main__":
     youtube_api = YouTubeApi("sbs 드라마")
+    test_video_id = "JQ2aoqwHmgE"
     # print(youtube_api.get_video_ids())
     # print(youtube_api.get_video_list())
-    # youtube_api.get_video_script()
-    # youtube_api.get_reply()
-
-    youtube_api.get_video_thumbnail()
-    # youtube_api.get_video_inspect()
+    #
+    # print(youtube_api.get_video_script(test_video_id))
+    # print(youtube_api.get_reply(test_video_id))
+    #
+    # print(youtube_api.get_video_inspect(test_video_id))
+    #
+    # print(youtube_api.get_video_thumbnail(test_video_id))
+    # print(youtube_api.get_video_viewcount(test_video_id))
+    # print(youtube_api.get_video_like_count(test_video_id))
+    # print(youtube_api.get_video_url(test_video_id))
+    print(youtube_api.get_video_tag(test_video_id))
