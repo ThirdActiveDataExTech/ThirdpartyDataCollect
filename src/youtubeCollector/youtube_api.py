f06131aa9d212b48ba4e68724ad5f71b89cfdaeb
eunmiGo <148742489+eunmiGo@users.noreply.github.com>
2025-01-10 18:09:19 +0900

    [수집 항목 추가]
    - 영상 mp3 수집 기능 추가(get_video_mp3)

--- src/youtubeCollector/youtube_api.py
+++ src/youtubeCollector/youtube_api.py
@@ -1,5 +1,9 @@
+import os
+
 import googleapiclient.discovery
 import googleapiclient.errors
+import yt_dlp
+
 import config.conf as config
 
 from youtube_transcript_api import YouTubeTranscriptApi
@@ -29,6 +33,30 @@ def get_video_script(video_id):
     return sentences
 
 
+def get_video_url(video_id):
+    url = "https://www.youtube.com/watch?v="+video_id
+    return url
+
+
+def get_video_mp3(video_id):
+    url = get_video_url(video_id)
+    output_dir = os.path.join('./tmp/', 'mp3test', '%(title)s.%(ext)s')
+    try:
+        ydl_opts = {  # youtube_dl 라이브러리 설정
+            'outtmpl': output_dir,
+            'format': 'bestaudio/best',  # 최고 품질로 추출
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',  # 영상을 오디오 파일로 추출
+                'preferredcodec': 'mp3',  # 오디오 파일 포맷을 mp3 파일로 설정
+                'preferredquality': '192', #오디오 품질 설정 192k
+            }],
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+    except Exception as e:
+        print(f"Error occurred: {e}")
+
+
 class YouTubeApi:
     def __init__(self, search_term):
         self.search_term = search_term
@@ -56,7 +84,7 @@ class YouTubeApi:
         video_ids = [item["id"]["videoId"] for item in video_list["items"]]
         return video_ids
 
-    def get_reply(self, video_id):
+    def get_reply(self):
         request = self.youtube.commentThreads().list(
             part="snippet,replies",
             maxResults=5,
@@ -97,10 +125,10 @@ class YouTubeApi:
 if __name__ == "__main__":
     youtube_api = YouTubeApi("sbs 드라마")
     test_video_id = "JQ2aoqwHmgE"
-    # print(youtube_api.get_video_ids())
     # print(youtube_api.get_video_list())
+    # print(youtube_api.get_video_ids())
     #
-    # print(youtube_api.get_video_script(test_video_id))
+    # print(get_video_script(test_video_id))
     # print(youtube_api.get_reply(test_video_id))
     #
     # print(youtube_api.get_video_inspect(test_video_id))
@@ -109,4 +137,6 @@ if __name__ == "__main__":
     # print(youtube_api.get_video_viewcount(test_video_id))
     # print(youtube_api.get_video_like_count(test_video_id))
     # print(youtube_api.get_video_url(test_video_id))
-    print(youtube_api.get_video_tag(test_video_id))
+    # print(youtube_api.get_video_tag(test_video_id))
+    # print(get_video_url(test_video_id))
+    get_video_mp3(test_video_id)
